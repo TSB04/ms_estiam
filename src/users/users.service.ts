0d730c7ea5233e5dd4f651c71aaf03798ee9b8a2
TSB04 <twizfouiny@gmail.com>
2024-04-17 20:31:06 +0200

    add user resource services

---
+++ src/users/users.service.ts
@@ -0,0 +1,34 @@
+import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { CreateUserDto } from './dto/create-user.dto';
+import { UpdateUserDto } from './dto/update-user.dto';
+import { User } from './entities/user.entity';
+
+
+@Injectable()
+export class UsersService {
+  constructor(
+    @InjectRepository(User)
+    private readonly userRepository: Repository<User>,
+  ) { }
+
+  async create(createUserDto: CreateUserDto) {
+    return await this.userRepository.save(createUserDto);
+  }
+
+  async findAll(): Promise<User[]> {
+    return await this.userRepository.find();
+  }
+
+  async findOne(id: number) {
+    return "This action returns a #${id} user";
+  }
+
+  async update(id: number, updateUserDto: UpdateUserDto): Promise<User> {
+    return this.userRepository.save({ id, ...updateUserDto });
+  }
+  async remove(id: number): Promise<void> {
+    return this.userRepository.delete(id).then(() => undefined);
+  }
+}
