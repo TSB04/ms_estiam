4147e794922bdb15dc83c8da3c899d232f76180c
Ala Trojette <ala.trojette35@gmail.com>
2024-04-19 21:10:27 +0100

    linked front to backend

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,24 +4,31 @@ import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AuthModule } from './auth/auth.module';
+import { RestaurentModule } from './restaurent/restaurent.module';
+import { PlatModule } from './plat/plat.module';
+import { CommandeModule } from './commande/commande.module';
+import { LivreurModule } from './livreur/livreur.module';
 
 @Module({
-  imports: [TypeOrmModule.forRoot(
-    {
-      "type": "mysql",
-      "host": "127.0.0.1",
-      "port": 3306,
-      "username": "root",
-      "password": "tsb0408",
-      "database": "msdb",
-      "entities": ["dist/**/*.entity{.ts,.js}"],
-      "synchronize": true,
-    }
-
-  ),
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: '127.0.0.1',
+      port: 3306,
+      username: 'root',
+      password: 'admin',
+      database: 'dbms',
+      entities: ['dist/**/*.entity{.ts,.js}'],
+      synchronize: true,
+    }),
     UsersModule,
-    AuthModule],
+    AuthModule,
+    RestaurentModule,
+    PlatModule,
+    CommandeModule,
+    LivreurModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule { }
+export class AppModule {}
