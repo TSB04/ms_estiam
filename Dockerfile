e874c25220ee2f7eecdb82d6c1159a2af7fb7a85
TSB04 <twizfouiny@gmail.com>
2024-05-16 13:14:26 +0200

    ready for prod

--- Dockerfile
+++ Dockerfile
@@ -1,15 +1,23 @@
+# Use the official Node.js image as a base image
 FROM node:lts-alpine
 
-ENV NODE_ENV=production
-
+# Set the working directory in the container
 WORKDIR /usr/src/app
 
-COPY ["package.json", "package-lock.json*", "npm-shrinkwrap.json*", "./"]
+# Copy package.json and package-lock.json
+COPY package*.json ./
 
-RUN npm install -g @nestjs/cli && npm install --production --silent && mv node_modules ../
+# Install dependencies
+RUN npm install -g @nestjs/cli && npm install --production
 
+# Copy the rest of the application code
 COPY . .
 
+# Build the NestJS application
+RUN npm run build
+
+# Expose the port the app runs on
 EXPOSE 4040
 
-CMD ["npm", "start"]
+# Start the application
+CMD ["node", "dist/main"]
--- src/main.ts
+++ src/main.ts
@@ -1,23 +1,33 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ConfigService } from '@nestjs/config';
-import {
-  BadRequestException,
-  ValidationError,
-  ValidationPipe,
-} from '@nestjs/common';
+import { ValidationPipe, BadRequestException, ValidationError } from '@nestjs/common';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule, { cors: true });
-  const appConfig: ConfigService = app.get(ConfigService);
-  const appPort: number = appConfig.get<number>('port');
+  const app = await NestFactory.create(AppModule);
+
+  configureCors(app);
+  configureGlobalPipes(app);
+
+  const configService = app.get(ConfigService);
+  const port = configService.get<number>('port') || 3000; // Provide a default port if not set
+
+  await app.listen(port);
+  console.log(`Application is running on: ${await app.getUrl()}`);
+}
+
+function configureCors(app) {
+  app.enableCors({
+    origin: 'https://ambitious-meadow-0f23cad03.5.azurestaticapps.net/',
+  });
+}
+
+function configureGlobalPipes(app) {
   app.useGlobalPipes(
     new ValidationPipe({
-      exceptionFactory: (errors: ValidationError[]) => {
-        return new BadRequestException(errors);
-      },
+      exceptionFactory: (errors: ValidationError[]) => new BadRequestException(errors),
     }),
   );
-  await app.listen(appPort);
 }
+
 bootstrap();
